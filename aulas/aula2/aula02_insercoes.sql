62065764765bfb8a2c483e8d5925d860dff1e030
Josecleber <josecleber0910@gmail.com>
2025-04-23 15:57:45 -0300

    Delete aula02_insercoes.sql

--- aulas/aula2/aula02_insercoes.sql
+++
@@ -1,4 +0,0 @@
--- Aula 2 - Inserção de dados
-INSERT INTO Tipo VALUES (1, 'Roupas'), (2, 'Alimentos'), (3, 'Eletrônicos');
-INSERT INTO Produto VALUES (101, 1, 150.00, 40), (102, 2, 10.00, 100), (103, 3, 2500.00, 25);
-INSERT INTO Usuario VALUES (201, 'José'), (202, 'João'), (203, 'Mariana');
