016a06ee98c4384a5b481ab48490e346227a1afa
Josecleber <josecleber0910@gmail.com>
2025-04-23 16:05:15 -0300

    Update aula05_funcoes1.sql

--- aulas/aula5/aula05_funcoes1.sql
+++ aulas/aula5/aula05_funcoes1.sql
@@ -1,1 +1,18 @@
--- Function com cursor - aula 5 (parte 1)
+-- Aula 5 - Function com Cursor
+CREATE FUNCTION CalculateOrderTotal (@orderNumber INT) RETURNS DECIMAL(10,2) AS
+BEGIN
+    DECLARE @totalAmount DECIMAL(10,2) = 0;
+    DECLARE @productCode VARCHAR(15), @quantity INT, @price DECIMAL(10,2);
+    DECLARE orderDetailsCursor CURSOR FOR
+    SELECT productCode, quantityOrdered, priceEach FROM orderdetails WHERE orderNumber = @orderNumber;
+    OPEN orderDetailsCursor;
+    FETCH NEXT FROM orderDetailsCursor INTO @productCode, @quantity, @price;
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        SET @totalAmount = @totalAmount + (@quantity * @price);
+        FETCH NEXT FROM orderDetailsCursor INTO @productCode, @quantity, @price;
+    END;
+    CLOSE orderDetailsCursor;
+    DEALLOCATE orderDetailsCursor;
+    RETURN @totalAmount;
+END;
