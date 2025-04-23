7b4ca2cdc69134f724e507743a72ac7434f7978a
Josecleber <josecleber0910@gmail.com>
2025-04-23 16:03:20 -0300

    Update aula04_procedure.sql

--- aulas/aula4/aula04_procedure.sql
+++ aulas/aula4/aula04_procedure.sql
@@ -1,1 +1,17 @@
--- Procedure com cursor - aula 4
+-- Aula 4 - Procedure com Cursor
+CREATE PROCEDURE UpdateStockQuantity @totalQuantityInStock DECIMAL(10,2) OUTPUT AS
+BEGIN
+    DECLARE @productCode VARCHAR(15), @quantityInStock DECIMAL(10,2);
+    SET @totalQuantityInStock = 0;
+    DECLARE productCursor CURSOR FOR SELECT productCode, quantityInStock FROM products;
+    OPEN productCursor;
+    FETCH NEXT FROM productCursor INTO @productCode, @quantityInStock;
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        IF @quantityInStock <= 1000
+            SET @totalQuantityInStock = @totalQuantityInStock + @quantityInStock;
+        FETCH NEXT FROM productCursor INTO @productCode, @quantityInStock;
+    END
+    CLOSE productCursor;
+    DEALLOCATE productCursor;
+END;
